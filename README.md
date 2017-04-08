26777e33795f9f3801b87ac1ea8262755cbc7927
Austin Pearce <austinpearce1005@gmail.com>
2017-04-08 02:27:28 -0400

    Add documentation and refactor library into Javascript class

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # ansi
+
+## Usage
+```Javascript
+const ANSI = require('./ansi.js');
+const ansi = new ANSI(process.stdout);
+```
+
+### .moveTo(x, y)
+Moves the cursor to the position specified by `(x, y)`. **These values are 1-based (The top left corner is (1, 1))**
+
+### .up(y)
+Moves the cursor up `y` units. Does not have any effect if the cursor is at the edge of the screen.
+
+### .down(y)
+Moves the cursor down `y` units. Does not have any effect if the cursor is at the edge of the screen.
+
+### .forward(x)
+Moves the cursor to the right `x` units. Does not have any effect if the cursor is at the edge of the screen.
+
+### .backward(x)
+Moves the cursor to the left `x` units. Does not have any effect if the cursor is at the edge of the screen.
+
+### .save()
+Saves the cursor position
+
+### .restore()
+Restores the cursor position
+
+### .eraseLine()
+Erases the contents of the current line the cursor is on
+
+### .eraseDisplay()
+Erases the contents of the whole display
+
+### .color(color, bg)
+Sets the color of the terminal at the position of the cursor. Holds its effect until the colors are reset. Either argument can be omitted to use only one or the other.
+
+| Colors  |
+-----------
+| Black   |
+| Red     |
+| Green   |
+| Yellow  |
+| Blue    |
+| Magenta |
+| Cyan    |
+| White   |
+
+### .reset()
+Resets the terminal colors to the default
---
+++ ansi.js
@@ -0,0 +1,87 @@
+class ANSI {
+  constructor(out) {
+    this.o = out;
+    this.escape = '\x1b[';
+    this.colors = {
+  		'black': [30, 40],
+  		'red': [31, 41],
+  		'green': [32, 42],
+  		'yellow': [33, 43],
+  		'blue': [34, 44],
+  		'magenta': [35, 45],
+  		'cyan': [36, 46],
+  		'white': [37, 47]
+  	}
+    this.columns = out.columns;
+    this.rows = out.rows;
+    out.on('resize', () => {
+      this.columns = out.columns;
+      this.rows = out.rows;
+    });
+  }
+  checkBounds(x, y) {
+    let withinBounds = true;
+    if(x) {
+      if(x < 1 || x > this.columns) {
+        withinBounds = false;
+      }
+    }
+    if(y) {
+      if(y < 1 || y > this.rows) {
+        withinBounds = false;
+      }
+    }
+    return withinBounds;
+  }
+	moveTo(x, y) {
+		if(withinBounds(x, y)){
+			this.o.write(this.escape + x + ';' + y + 'H');
+		}
+	}
+	up(y) {
+		if(withinBounds(1, y)){
+			this.o.write(this.escape + y + 'A');
+		}
+	}
+	down(y) {
+		if(withinBounds(1, y)){
+			this.o.write(this.escape + y + 'B');
+		}
+	}
+	forward(x) {
+		if(withinBounds(x, 1)){
+			this.o.write(this.escape + x + 'C');
+		}
+	}
+	backward(x){
+		if(withinBounds(x, 1)){
+			this.o.write(this.escape + x + 'D');
+		}
+	}
+	save() {
+		this.o.write(this.escape + 's');
+	}
+	restore() {
+		this.o.write(this.escape + 'u');
+	}
+	eraseDisplay() {
+		this.o.write(this.escape + '2J');
+	}
+	eraseLine() {
+		this.o.write(this.escape + 'K');
+	}
+	color(color, bg) {
+		if(this.colors.hasOwnProperty(color) && this.colors.hasOwnProperty(bg)){
+			this.o.write(this.escape + this.colors[color][0] + ';' + this.colors[bg][1] + 'm');
+		}else if(this.colors.hasOwnProperty(color)){
+			this.o.write(this.escape + this.colors[color][0] + 'm');
+		}else if(this.colors.hasOwnProperty(bg)){
+			this.o.write(this.escape + this.colors[bg][1] + 'm');
+		}
+	}
+	reset() {
+		this.o.write(this.escape + '0m');
+	}
+}
+
+module.exports = ANSI;
